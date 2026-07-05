a531ae289c56d88ce02afdb892a9ee2d43b1c563
stik3r <maxon.07@mail.ru>
2026-07-05 23:57:46 +0300

    init

---
+++ src/main/java/org/moichekionline/parcer/ParcerApplication.java
@@ -0,0 +1,13 @@
+package org.moichekionline.parcer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcerApplication.class, args);
+    }
+
+}
